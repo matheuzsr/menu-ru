a4a7c86175693fb1d1ebf4bc355bc3280b784618
matheuzsr <matheusdasilvaroch@gmail.com>
2022-08-16 22:04:37 -0300

    Corrigindo tipagem

--- src/utils/separateWordsMenu.ts
+++ src/utils/separateWordsMenu.ts
@@ -14,24 +14,22 @@ export const separateText = (texto: string) => {
     return [initiator, end]
   }
 
-  const delimitadores: any[] = labels?.map((label) => {
-    return getDelimiters(label, texto)
-  })
+  const delimitadores = (idDelimitador: number): any => {
+    return labels?.map((label) => {
+      return getDelimiters(label, texto)[idDelimitador]
+    })
+  }
 
   function getLabelText(texto:string, delimitador1: any, delimitador2 = []) {
     return texto.slice(delimitador1[1], delimitador2[0]).split("\n").join("")
   }
 
-  if(!delimitadores) {
-    return {}
-  }
-  
   return {
-    entrada: getLabelText(texto, delimitadores[0], delimitadores[1]),
-    pratoPrincipal: getLabelText(texto, delimitadores[1], delimitadores[2]),
-    opcao: getLabelText(texto, delimitadores[2], delimitadores[3]),
-    acompanhamento: getLabelText(texto, delimitadores[3], delimitadores[4]),
-    guarnicao: getLabelText(texto, delimitadores[4], delimitadores[5]),
-    sobremesa: getLabelText(texto, delimitadores[5], delimitadores[6]).split('*')[0],
+    entrada: getLabelText(texto, delimitadores(0), delimitadores(1)),
+    pratoPrincipal: getLabelText(texto, delimitadores(1), delimitadores(2)),
+    opcao: getLabelText(texto, delimitadores(2), delimitadores(3)),
+    acompanhamento: getLabelText(texto, delimitadores(3), delimitadores(4)),
+    guarnicao: getLabelText(texto, delimitadores(4), delimitadores(5)),
+    sobremesa: getLabelText(texto, delimitadores(5), delimitadores(6)).split('*')[0],
   }
 }
